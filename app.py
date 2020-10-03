125a937f6ce4e75281956c46ed5a298b99376b81
Rohith Kalluru <rohithkalluru@gmail.com>
2020-10-03 09:50:50 +0530

    project minimum requirements satisfied and styling is pending

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import requests
+import json
 
 from flask import Flask,render_template,request,session,redirect,url_for,flash
 from flask_session import Session
@@ -43,6 +45,7 @@ if not os.getenv("DATABASE_URL"):
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
+app.config["DATABASE_URL"] = os.getenv("DATABASE_URL")
 app.config["SESSION_TYPE"] = "filesystem"
 app.config['development'] = True
 app.config['SECRET_KEY'] = '#KR#'
@@ -65,12 +68,29 @@ class Users(Base):
     pwrd = Column(String)
     date = Column(DateTime)
 
+class Reviews(Base):
+    __tablename__ = 'REVIEWS'
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    review = Column(String)
+    rating = Column(String)
+    fname = Column(String)
+    date = Column(DateTime)
+    email = Column(String,ForeignKey('USERS.email'))
+    isbn = Column(String,ForeignKey('BOOKS.isbn'))
+
+class Books(Base):
+    __tablename__ = "BOOKS"
+    isbn = Column(String, primary_key=True, nullable=False)
+    title = Column(String)
+    author = Column(String)
+    year = Column(Integer)
+
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return redirect(url_for('home'))
 
 @app.route("/home")
-def userHome():
+def home():
     if 'email' in session:
         return render_template('index.html', email=session['email'])
     return render_template('layout.html',email = None)
@@ -107,9 +127,10 @@ def registration():
             if name is not None and name.email == remail and name.pwrd == rpassword:
                 print('session created')
                 session['email'] = name.email
+                session['fname'] = name.fname
                 user = name.email
                 # s = session['email']
-                return render_template('index.html', email=user)
+                return render_template('search.html', email=user)
             elif name is not None and name.email == remail and name.pwrd != rpassword:
                 print('Incorrect password, try again')
                 # session['email'] = name.email
@@ -174,7 +195,11 @@ def search():
     if isbn!="" and title=="" and author=="":
         try:
             db = scoped_session(sessionmaker(bind=engine))
-            query = db.query(Books).filter(Books.isbn == isbn)
+            isbn = '%'+isbn+'%'
+            session['isbn'] = isbn
+            session['title'] = None
+            session['author'] = None
+            query = db.query(Books).filter(Books.isbn.ilike(isbn))
             if query != None:
                 # r = query.all()
                 # print(r)
@@ -192,7 +217,11 @@ def search():
     elif isbn=="" and title!="" and author=="":
         try:
             db = scoped_session(sessionmaker(bind=engine))
-            query = db.query(Books).filter(Books.title == title)
+            title = '%'+title+'%'
+            session['title'] = title
+            session['author'] = None
+            session['isbn'] = None
+            query = db.query(Books).filter(Books.title.ilike(title))
             if query != None:
                 # r = query.all()
                 # print(r)
@@ -210,7 +239,11 @@ def search():
     elif isbn=="" and title=="" and author!="":
         try:
             db = scoped_session(sessionmaker(bind=engine))
-            query = db.query(Books).filter(Books.author == author)
+            author = '%'+author+'%'
+            session['author'] = author
+            session['isbn'] = None
+            session['title'] = None
+            query = db.query(Books).filter(Books.author.ilike(author))
             if query != None:
                 # r = query.all()
                 # print(r)
@@ -228,7 +261,12 @@ def search():
     elif isbn!="" and title!="" and author=="":
         try:
             db = scoped_session(sessionmaker(bind=engine))
-            query = db.query(Books).filter(Books.title == title,Books.isbn == isbn)
+            title = '%'+title+'%'
+            isbn = '%'+isbn+'%'
+            session['title'] = title
+            session['isbn'] = isbn
+            session['author'] = None
+            query = db.query(Books).filter(or_(Books.title.ilike(title),Books.isbn.ilike(isbn)))
             if query != None:
                 # r = query.all()
                 # print(r)
@@ -246,7 +284,12 @@ def search():
     elif isbn!="" and title=="" and author!="":
         try:
             db = scoped_session(sessionmaker(bind=engine))
-            query = db.query(Books).filter(Books.author == author,Books.isbn == isbn)
+            isbn = '%'+isbn+'%'
+            author = '%'+author+'%'
+            session['author'] = author
+            session['isbn'] = isbn
+            session['title'] = None
+            query = db.query(Books).filter(or_(Books.isbn.ilike(isbn),Books.author.ilike(author)))
             if query != None:
                 # r = query.all()
                 # print(r)
@@ -264,7 +307,12 @@ def search():
     elif isbn=="" and title!="" and author!="":
         try:
             db = scoped_session(sessionmaker(bind=engine))
-            query = db.query(Books).filter(Books.title == title,Books.author == author)
+            title = '%'+title+'%'
+            author = '%'+author+'%'
+            session['title'] = title
+            session['author'] = isbn
+            session['isbn'] = None
+            query = db.query(Books).filter(or_(Books.title.ilike(title),Books.author.ilike(author)))
             if query != None:
                 # r = query.all()
                 # print(r)
@@ -282,7 +330,13 @@ def search():
     else:
         try:
             db = scoped_session(sessionmaker(bind=engine))
-            query = db.query(Books).filter(Books.title == title,Books.author == author,Books.isbn == isbn)
+            isbn = '%'+isbn+'%'
+            title = '%'+title+'%'
+            author = '%'+author+'%'
+            session['title'] = title
+            session['isbn'] = isbn
+            session['author'] = author
+            query = db.query(Books).filter(or_(Books.title.ilike(title),Books.author.ilike(author),Books.isbn.ilike(isbn)))
             if query != None:
                 # r = query.all()
                 # print(r)
@@ -298,9 +352,117 @@ def search():
         finally:
             db.close()
 
+@app.route("/searchtest")
+def searchtest():
+    return render_template('search.html',email=session['email'])
+
 @app.route("/booksearch/<string:isbn>")
-def booksearch():
-    pass
+def booksearch(isbn):
+    db = scoped_session(sessionmaker(bind=engine))
+    try:
+        if not engine.dialect.has_table(engine, "REVIEWS"):  # If table don't exist, Create.
+            Reviews.__table__.create(bind=engine, checkfirst=True)
+        # query = db.query(Books).filter(Books.isbn==isbn)
+        query = db.query(Books).filter(Books.isbn==isbn)
+        if query != None:
+            # r = query.all()
+            # print(r)
+            # for book in r:
+            #     print(f"added{book.title} with number {book.isbn} written by {book.author} published in the year {book.year}")
+            booksquery = db.query(Reviews).filter(Reviews.isbn==isbn)
+            print(booksquery)
+            res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "aLvwXAjKk7bi8mYKzi0mw", "isbns": isbn})
+            data = res.text
+            parsed = json.loads(data)
+            print(parsed)
+            res = {}
+            for i in parsed:
+                for j in (parsed[i]):
+                    res = j
+            return render_template('bookdetails.html',book = query.first(),email=session['email'],res = res,isbn=isbn,review=booksquery.all())
+        else :
+            flash("there are no books available with the specified details,try with more specific details")
+            return render_template('search.html',email=session['email'])
+    except SQLAlchemyError as e:
+        print(e)
+        return render_template('fail.html',path='./static/css/styles.min.css')
+    finally:
+        db.close()
+
+@app.route("/review/<string:isbn>",methods=["POST"])
+def review(isbn):
+    email = session['email']
+    print(email)
+    now = datetime.now()
+    rating = request.form['rating']
+    review = request.form.get("review")
+    fname = session['fname']
+    print(fname)
+    author = session['author']
+    print("author =",session['author'])
+    title = session['title']
+    print("title =",session['title'])
+    try:
+        db = scoped_session(sessionmaker(bind=engine))
+        query = db.query(Reviews).filter(Reviews.email==email)
+
+        if query != None:
+            rbooks = []
+            for r in query.all():
+                rbooks.append(r.isbn)
+            bquery = book_query(isbn,author,title)
+            if isbn not in rbooks:
+                # query = db.query(Users).filter(Users.email==email)
+                row = Reviews(email=email,fname=fname,review=review,date=now,isbn=isbn,rating=rating)
+                db.add(row)
+                db.commit()
+                flash("Review Submitted")
+                return render_template('books.html',row = bquery,email=session['email'])
+            else :
+                flash("You already reviewed the book, try to review another book")
+                return render_template('books.html',row = bquery,email=session['email'])
+        else:
+            row = Reviews(email=email,fname=fname,review=review,date=now,isbn=isbn,rating=rating)
+            db.add(row)
+            db.commit()
+            bquery = book_query(isbn,author,title)
+            flash("Review Submitted")
+            return render_template('books.html',row = bquery,email=session['email'])
+    except SQLAlchemyError as e:
+        print(e)
+        return render_template('fail.html',path='./static/css/styles.min.css')
+    finally:
+        db.close()
+
+def book_query(isbn,author,title):
+    try:
+        db = scoped_session(sessionmaker(bind=engine))
+        bquery=None
+        if session['title']!=None and session['isbn']!=None and session['author']!=None:
+            bquery = db.query(Books).filter(or_(Books.title.ilike(title),Books.author.ilike(author),Books.isbn.ilike(isbn)))
+        elif session['title']!=None and session['author']!=None:
+            bquery = db.query(Books).filter(or_(Books.title.ilike(title),Books.author.ilike(author)))
+        elif session['author']!=None and session['isbn']!=None:
+            bquery = db.query(Books).filter(or_(Books.isbn.ilike(isbn),Books.author.ilike(author)))
+        elif session['title']!=None and session['isbn']!=None:
+            bquery = db.query(Books).filter(or_(Books.title.ilike(title),Books.isbn.ilike(isbn)))
+        elif session['isbn']!=None:
+            bquery = db.query(Books).filter(Books.isbn.ilike(isbn))
+        elif session['title'] !=None:
+            bquery = db.query(Books).filter(Books.title.ilike(title))
+        else:
+            bquery = db.query(Books).filter(Books.author.ilike(author))
+    except SQLAlchemyError as e:
+        print(e)
+        return render_template('fail.html',path='./static/css/styles.min.css')
+    finally:
+        db.close()
+
+    if bquery == None:
+        return None
+
+    return bquery.all()
+
 
 @app.route("/login",methods=["POST"])
 def login():
@@ -308,6 +470,7 @@ def login():
 
 @app.route("/login_form")
 def login_form():
+    session.clear()
     session.pop('email', None)
     return render_template('index.html',path='./static/css/styles.min.css', email=None)
 
